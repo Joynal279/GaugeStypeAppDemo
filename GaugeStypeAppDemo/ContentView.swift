663797c2bc956a6a6dbf3deca00044de24c7665f
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-09 07:18:59 +0600

    Spedometer view added

--- GaugeStypeAppDemo/ContentView.swift
+++ GaugeStypeAppDemo/ContentView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(content: {
+            SpeedoMeterView()
+                .padding()
+        })
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+//1
+struct ContentView1: View {
+    @State private var progress = 0.7
+    var body: some View {
+        Gauge(value: progress) {
+            Text("Upload Status")
+        }
+    }
+}
+
+//2
+struct ContentView2: View {
+    
+    @State private var progress = 0.7
+    
+    var body: some View{
+        Gauge(value: progress) {
+            Text("Upload Status")
+        } currentValueLabel: {
+            Text(progress.formatted(.percent))
+        } minimumValueLabel: {
+            Text(0.formatted(.percent))
+        } maximumValueLabel: {
+            Text(100.formatted(.percent))
+        }
+        .padding()
+    }
+}
+
+//3
+struct SpeedoMeterView: View {
+    
+    @State private var currentSpeed = 100.0
+    
+    var body: some View {
+        Gauge(value: currentSpeed, in: 0...200) {
+            Text("Speed")
+        } currentValueLabel: {
+            Text("\(currentSpeed.formatted(.number))km/h")
+        } minimumValueLabel: {
+            Text(0.formatted(.number))
+        }maximumValueLabel: {
+            Text(200.formatted(.number))
+        }
+    }
 }
