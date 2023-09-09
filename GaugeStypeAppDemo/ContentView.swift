72350081a5db73102d5e0b5825266a261e1d2e89
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-09 07:54:38 +0600

    gauge style implemented

--- GaugeStypeAppDemo/ContentView.swift
+++ GaugeStypeAppDemo/ContentView.swift
@@ -87,5 +87,45 @@ struct CustomImageView: View {
         }maximumValueLabel: {
             Text(200.formatted(.number))
         }
+        .tint(.yellow)
+        .gaugeStyle(.accessoryCircularCapacity)
     }
 }
+
+//5
+struct SpeedometerGaugeStyle: GaugeStyle {
+    private var purpleGradient = LinearGradient(gradient: Gradient(colors: [ Color(red: 207/255, green: 150/255, blue: 207/255), Color(red: 107/255, green: 116/255, blue: 179/255) ]), startPoint: .trailing, endPoint: .leading)
+ 
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+ 
+            Circle()
+                .foregroundColor(Color(.systemGray6))
+ 
+            Circle()
+                .trim(from: 0, to: 0.75 * configuration.value)
+                .stroke(purpleGradient, lineWidth: 20)
+                .rotationEffect(.degrees(135))
+ 
+            Circle()
+                .trim(from: 0, to: 0.75)
+                .stroke(Color.black, style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .round, dash: [1, 34], dashPhase: 0.0))
+                .rotationEffect(.degrees(135))
+ 
+            VStack {
+                configuration.currentValueLabel
+                    .font(.system(size: 80, weight: .bold, design: .rounded))
+                    .foregroundColor(.gray)
+                Text("KM/H")
+                    .font(.system(.body, design: .rounded))
+                    .bold()
+                    .foregroundColor(.gray)
+            }
+ 
+        }
+        .frame(width: 300, height: 300)
+ 
+    }
+ 
+}
+
